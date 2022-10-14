ba8df840943228ba4f5f57b7a7f7871f673519f5
NhatPhat1999 <tonhatphat1403@gmail.com>
2022-10-14 22:34:55 +0700

    bc_39_buoi1

---
+++ main.js
@@ -0,0 +1,57 @@
+// bt1
+// * - Đầu vào:
+const salaryPerday = 100000;
+var dayWorked = 5;
+//* - xử lý
+var result = "tiền lương: " + salaryPerday * dayWorked;
+//* - Đầu ra:
+console.log(result);
+
+
+// bt2
+// * - Đầu vào:
+var so1 = 2;
+var so2 = 3;
+var so3 = 4;
+var so4 = 5;
+var so5 = 6;
+//* - xử lý
+var tong = so1 + so2 + so3 + so4 + so5;;
+var result = "giá trị trung bình: " + tong / 5;
+//* - Đầu ra
+console.log(result);
+
+
+// bt3
+// * - Đầu vào:
+const donGia = 23500;
+var soTienusd = 5;
+//* - xử lý
+var soTienvnd = soTienusd * donGia;
+var result = "số tiền quy đổi VND: " + soTienvnd + " VND";
+//* - Đầu ra
+console.log(result);
+
+
+// bt4
+// * - Đầu vào:
+var chieuRong = 12;
+var chieuDai = 12;
+//* - xử lý
+var chuVi = "chu vi hcn: " + (chieuDai + chieuRong) * 2;
+var dienTich = ", dien tích hcn:" + chieuDai * chieuRong;
+var result = chuVi + dienTich;
+//* - Đầu ra
+console.log(result);
+
+
+// bt5
+// * - Đầu vào:
+var so2chuso = 36;
+//* - xử lý
+var soHangdv = so2chuso % 10;
+var soHangchuc = Math.floor(so2chuso / 10);
+result = soHangdv + soHangchuc;
+//* - Đầu ra
+console.log(result);
+
